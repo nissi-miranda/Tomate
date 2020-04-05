73477416a6fe66e120212398c848c069b9c18bac
Nissi Miranda <nissivm@yahoo.com.br>
2020-04-05 13:10:16 +0100

    Added newly created Xcode project

---
+++ Tomate/Tomate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tomate
+//
+//  Created by Nissi Vieira Miranda on 05/04/2020.
+//  Copyright © 2020 Nissi Vieira Miranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
