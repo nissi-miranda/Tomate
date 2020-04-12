4b21bfd07964a88d6e61e9f58b28c49e8c4e86c7
Nissi Miranda <nissivm@yahoo.com.br>
2020-04-12 19:30:57 +0100

    Changes in test classes

--- Tomate/TomateTests/TomateTests.swift
+++ Tomate/TomateTests/TomateTests.swift
@@ -11,24 +11,25 @@ import XCTest
 
 class TomateTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
     }
 
-    func testExample() throws {
+    func testOne() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testTwo() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
+    
+    func testThree() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
 }
